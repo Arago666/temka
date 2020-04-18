013b3ad0dac42ff1e4bd508068918cdbb875d6a5
artem <art.manaenkov@gmail.com>
2020-04-18 12:16:03 +0700

    fixBroadCastingConfig4

--- public/js/app.js
+++ public/js/app.js
@@ -62202,9 +62202,11 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
-  key: 'anyKey',
+  key: "anyKey",
+  cluster: "mt1",
   wsHost: window.location.hostname,
   wsPort: 6001,
+  wssPort: 6001,
   disableStats: true,
   enabledTransports: ['ws', 'wss']
 });
