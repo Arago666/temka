27965fdb29c70d331243bd8f00b62bea8cc48497
artem <art.manaenkov@gmail.com>
2020-05-06 22:24:45 +0700

    copy Temka1

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -39,7 +39,7 @@ window.Echo = new Echo({
     wsHost: window.location.hostname,
     wsPort: 6001,
     disableStats: true,
-
+    // enabledTransports: ['ws', 'wss'],
 });
 
 window.Echo.channel('DemoChannel')
